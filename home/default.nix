b34d59ff23cd2d724fcb057b0354ba7ae680c767
Reginleif88 <git@reginleif.io>
2026-05-25 02:31:09 +0200

    Revert "stremio: patch embedded mpv via LD_PRELOAD for smooth playback"
    This reverts commit 072e1f330ed2e280858a90519b4e1960e4fcf1a9.

--- home/default.nix
+++ home/default.nix
@@ -25,9 +25,6 @@ in
     ./apps.nix
     ./browser.nix
     ./mpv.nix
-  ] ++ lib.optionals (hostname == "hyacinth") [
-    # NVIDIA box: patch Stremio's embedded mpv for display-synced playback.
-    ./stremio.nix
   ];
 
   home.username = "reginleif88";
